ce10f42a44eb4d7e1837934301689f39af1ab747
Chaedong.IM <88099590+Chaedie@users.noreply.github.com>
2022-05-10 11:24:34 +0900

    Time: 236 ms (49.11%), Space: 15.7 MB (18.29%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -4,24 +4,36 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # no copy array
-        start = 0
-        end = 0
-        n = len(nums)
+        # my First sol
+#         start = 0
+#         end = 0
+#         n = len(nums)
 
+#         for i in range(n):
+#             if start >= n:
+#                 break
+#             if nums[start] == 0:
+#                 while True:    
+#                     end += 1
+#                     if end >= n:
+#                         break
+#                     if nums[end] != 0:
+#                         nums[start], nums[end] = nums[end], nums[start]
+#                         break
+#             if nums[start] != 0:
+#                 start += 1
+#                 end = start
+                
+        # better Sol
+        n = len(nums)
+        indexNonZero = 0
         for i in range(n):
-            if start >= n:
-                break
-            if nums[start] == 0:
-                while True:    
-                    end += 1
-                    if end >= n:
-                        break
-                    if nums[end] != 0:
-                        nums[start], nums[end] = nums[end], nums[start]
-                        break
-            if nums[start] != 0:
-                start += 1
-                end = start
+            if nums[i] != 0:
+                nums[indexNonZero] = nums[i]
+                indexNonZero += 1
+        for i in range(indexNonZero, n):
+            nums[i] = 0
+                
                     
                 
            
