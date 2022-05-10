f001562b16bfad02973217803ddd8c13333383d3
Chaedong.IM <88099590+Chaedie@users.noreply.github.com>
2022-05-10 15:49:41 +0900

    Time: 311 ms (26.74%), Space: 18.5 MB (18.45%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -3,10 +3,16 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        # 1st sol
+        # n = len(s)
+        # start = 0
+        # end = n - 1
+        # for _ in range(n // 2):
+        #     s[start], s[end] = s[end], s[start]
+        #     start += 1
+        #     end -= 1
+            
+        # 2nd sol
         n = len(s)
-        start = 0
-        end = n - 1
-        for _ in range(n // 2):
-            s[start], s[end] = s[end], s[start]
-            start += 1
-            end -= 1
+        for i in range(n // 2):
+            s[i], s[n - 1 - i] = s[n - 1 - i], s[i]
