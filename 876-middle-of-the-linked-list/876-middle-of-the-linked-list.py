d90889e47e87af79663564ddeaf1bb841ae3cfdf
Chaedong.IM <88099590+Chaedie@users.noreply.github.com>
2022-05-12 08:32:36 +0900

    Time: 43 ms (46.36%), Space: 13.8 MB (95.73%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -16,3 +16,15 @@ class Solution:
         for _ in range(midPointer):
             head = head.next
         return head
+    
+        #Leetcode solution 2 
+        
+        # Fast is going faster twice of slow
+        # if fast is reach end of the linkedList
+        # then slow is reach mid of the linkedList
+        
+        # slow = fast = head
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+        # return slow
