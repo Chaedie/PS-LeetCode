90f6729ccd03cfb36d8a7b2f1f4a90d4f1916757
Chaedong.IM <88099590+Chaedie@users.noreply.github.com>
2022-05-13 08:36:15 +0900

    Time: 0 ms (100.00%), Space: 53.9 MB (63.45%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        int mid;
+
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                left = mid + 1;
+                continue;
+            }
+            if (nums[mid] > target) {
+                right = mid - 1;
+                continue;
+            }
+            
+        }
+        return -1;
+        
+    }
+}
